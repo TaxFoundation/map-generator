8d7113f6115ec0a72acc6d217433abb5d27c9adf
tvanantwerp <tom@tomvanantwerp.com>
2017-10-31 14:44:08 -0400

    Add basic data type selector

--- src/components/controls/DataControls.js
+++ src/components/controls/DataControls.js
@@ -1,8 +1,44 @@
 import React, { Component } from 'react';
+import MenuItem from 'material-ui/MenuItem';
 import RaisedButton from 'material-ui/RaisedButton';
+import SelectField from 'material-ui/SelectField';
 
 class DataControls extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      fileUploaded: false,
+      dataType: 'sequential'
+    };
+  }
+
   render() {
+    const DataSettings = () => {
+      const types = ['sequential', 'divergent', 'qualitative'];
+
+      return (
+        <div>
+          <h3>Describe Your Data</h3>
+          <SelectField
+            autoWidth={true}
+            floatingLabelText="What Type of Data is This?"
+            onChange={(e, i, v) => {this.setState({dataType: v});}}
+            value={this.state.dataType}
+          >
+            { types.map((t) => {
+              return (
+                <MenuItem
+                  value={t}
+                  primaryText={t.charAt(0).toLocaleUpperCase() + t.slice(1)}
+                />
+              );
+            })}
+          </SelectField>
+        </div>
+      );
+    };
+
     return (
       <div className="controls__data panel__section">
         <h3>Upload Data</h3>
@@ -26,6 +62,10 @@ class DataControls extends Component {
             }}
           />
         </RaisedButton>
+        { this.state.fileUploaded
+          ? <DataSettings />
+          : null
+        }
       </div>
     );
   }
