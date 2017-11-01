82c46e869f8fe56c10ed0d98348beac9dac2252a
Tom VanAntwerp <tom@tomvanantwerp.com>
2017-11-01 11:30:15 -0400

    Dynamic label colors based on contrast

--- src/containers/USMap.js
+++ src/containers/USMap.js
@@ -2,19 +2,19 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { geoAlbersUsa, geoPath } from 'd3-geo';
 import { feature } from 'topojson-client';
-import { colorScale } from '../helpers';
+import { colorScale, labelColor } from '../helpers';
 import Features from '../data/us.json';
 
 class USMap extends React.Component {
   constructor(props) {
     super(props);
-    
+
     this.scale = 800;
     this.xScale = 600;
     this.yScale = 400;
     this.xScalar = this.xScale / 600;
     this.yScalar = this.yScale / 400;
-    
+
     this.smallStates = {
       10: {
         x: 560 * this.xScalar,
@@ -34,27 +34,30 @@ class USMap extends React.Component {
   }
 
   render() {
-    const path = geoPath()
-      .projection(
-        geoAlbersUsa()
-          .scale(this.scale)
-          .translate([this.xScale/2, this.yScale/2 - 25])
-      );
-    const USDataFeatures = feature(Features, Features.objects[this.props.mapType]).features;
+    const path = geoPath().projection(
+      geoAlbersUsa()
+        .scale(this.scale)
+        .translate([this.xScale / 2, this.yScale / 2 - 25])
+    );
+    const USDataFeatures = feature(
+      Features,
+      Features.objects[this.props.mapType]
+    ).features;
 
-    const states = this.props.mapData.map((d) => {
+    const geographies = this.props.mapData.map(d => {
       // Match state's path to the current state data
-      let statePath = USDataFeatures.filter((s) => {
+      let statePath = USDataFeatures.filter(s => {
         return +s.id === +d.id;
       })[0];
 
+      let fill = colorScale(this.props.colors, this.props.domain)(d.value);
       let isSmallState = false;
       let smallStateRect;
       let label;
 
       if (this.props.mapType === 'states') {
         let center = path.centroid(statePath);
-  
+
         // Creat rect/label for small states
         if (d.id in this.smallStates) {
           isSmallState = true;
@@ -68,9 +71,9 @@ class USMap extends React.Component {
                 y={smallState.y}
                 height="16"
                 width="16"
-                fill={colorScale(this.props.colors, this.props.domain)(d.value)}
-                stroke='#ffffff'
-                strokeLinejoin='bevel'
+                fill={fill}
+                stroke="#ffffff"
+                strokeLinejoin="bevel"
               />
               <text
                 fontSize="12"
@@ -83,7 +86,7 @@ class USMap extends React.Component {
             </g>
           );
         }
-        
+
         // Create state labels
         let labelX = center[0];
         let labelY = center[1] + 6;
@@ -91,9 +94,10 @@ class USMap extends React.Component {
           labelX = this.labelOverrides[d.id].x;
           labelY = this.labelOverrides[d.id].y;
         }
-  
+
         label = (
           <text
+            fill={labelColor(fill)}
             fontSize="12"
             textAnchor="middle"
             x={labelX}
@@ -107,52 +111,42 @@ class USMap extends React.Component {
       return (
         <g key={`geo-${d.id}`}>
           <path
-            d={ path(statePath) }
+            d={path(statePath)}
             id={`state-${d.id}`}
-            className='state'
-            fill={colorScale(this.props.colors, this.props.domain)(d.value)}
-            stroke='#ffffff'
-            strokeLinejoin='bevel'
+            className="state"
+            fill={fill}
+            stroke="#ffffff"
+            strokeLinejoin="bevel"
           />
-          { this.props.mapType === 'states'
-            ? (isSmallState ? smallStateRect : label)
-            : null
-          }
+          {this.props.mapType === 'states'
+            ? isSmallState ? smallStateRect : label
+            : null}
         </g>
       );
     });
 
-    const legend = [...Array(this.props.steps).keys()].map((d) => {
+    const legend = [...Array(this.props.steps).keys()].map(d => {
       const keyGap = 10;
-      const keyWidth = (
-        (
-          (this.xScale / 2) / this.props.steps
-        ) - keyGap
-      ) * this.xScalar;
+      const keyWidth =
+        (this.xScale / 2 / this.props.steps - keyGap) * this.xScalar;
 
       return (
         <rect
           key={`legend-${d}`}
-          fill={
-            colorScale(this.props.colors, [0, this.props.steps - 1])(d)
-          }
+          fill={colorScale(this.props.colors, [0, this.props.steps - 1])(d)}
           height={20 * this.yScalar}
           width={keyWidth}
-          x={(this.xScale / 2) + ((keyWidth + keyGap) * d)}
+          x={this.xScale / 2 + (keyWidth + keyGap) * d}
           y={380 * this.yScalar}
-        ></rect>
+        />
       );
     });
 
     return (
       <div>
         <svg width="100%" viewBox={`0 0 ${this.xScale} ${this.yScale}`}>
-          <g className='states'>
-            { states }
-          </g>
-          <g className="legend">
-            { legend }
-          </g>
+          <g className="states">{geographies}</g>
+          <g className="legend">{legend}</g>
         </svg>
       </div>
     );
