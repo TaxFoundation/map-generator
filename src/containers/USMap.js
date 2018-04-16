db51258d292670dbd176f37c123ec4fe2ca806e9
Tom VanAntwerp <tom@tomvanantwerp.com>
2018-04-16 14:52:06 -0400

    Add proptypes to USMap

--- src/containers/USMap.js
+++ src/containers/USMap.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { geoAlbersUsa, geoPath } from 'd3-geo';
 import { feature } from 'topojson-client';
@@ -27,10 +28,7 @@ class USMap extends React.Component {
         .scale(this.scale)
         .translate([this.xScale / 2, this.yScale / 2 - 25])
     );
-    const USDataFeatures = feature(
-      Features,
-      Features.objects[this.props.mapType]
-    ).features;
+    const USDataFeatures = feature(Features, Features.objects[this.props.mapType]).features;
 
     const geographies = USDataFeatures.map(d => {
       // Match state's path to the current state data
@@ -43,17 +41,11 @@ class USMap extends React.Component {
       let abbr;
 
       if (data !== undefined) {
-        fill = colorScale(
-          this.props.colors,
-          this.props.domain,
-          data.value,
-          this.props.steps,
-          this.props.colorMode
-        );
+        fill = colorScale(this.props.colors, this.props.domain, data.value, this.props.steps, this.props.colorMode);
 
         if (this.props.mapType === 'states') {
           abbr = states.filter(s => +s.id === +data.id)[0]['abbr'];
-  
+
           // Creat rect/label for small states
           if (d.id in smallStateRects) {
             isSmallState = true;
@@ -71,39 +63,25 @@ class USMap extends React.Component {
             stroke={this.props.mapType === 'states' ? '#ffffff' : 'none'}
             strokeLinejoin="bevel"
           />
-          {this.props.mapType === 'states'
-            ? isSmallState
-              ? <SmallStateRect
-                smallState={smallStateRects[d.id]}
-                fill={fill}
-                abbr={abbr}
-              />
-              : <Label 
-                id={d.id}
-                fill={fill}
-                center={path.centroid(d)}
-                abbr={abbr}
-              />
-            : null}
+          {this.props.mapType === 'states' ? (
+            isSmallState ? (
+              <SmallStateRect smallState={smallStateRects[d.id]} fill={fill} abbr={abbr} />
+            ) : (
+              <Label id={d.id} fill={fill} center={path.centroid(d)} abbr={abbr} />
+            )
+          ) : null}
         </g>
       );
     });
 
     const legend = [...Array(this.props.steps).keys()].map(d => {
       const keyGap = 10;
-      const keyWidth =
-        (this.xScale / 2 / this.props.steps - keyGap) * this.xScalar;
+      const keyWidth = (this.xScale / 2 / this.props.steps - keyGap) * this.xScalar;
 
       return (
         <rect
           key={`legend-${d}`}
-          fill={colorScale(
-            this.props.colors,
-            [0, this.props.steps - 1],
-            d,
-            this.props.steps,
-            this.props.colorMode
-          )}
+          fill={colorScale(this.props.colors, [0, this.props.steps - 1], d, this.props.steps, this.props.colorMode)}
           height={20 * this.yScalar}
           width={keyWidth}
           x={this.xScale / 2 + (keyWidth + keyGap) * d}
@@ -117,14 +95,25 @@ class USMap extends React.Component {
         <svg width="100%" viewBox={`0 0 ${this.xScale} ${this.yScale}`}>
           <g className="geographies">{geographies}</g>
           <g className="legend">{legend}</g>
-          {this.props.mapType === 'counties'
-            ? <g className="state-outlines"><StateOutlines path={path} /></g>
-            : null}
+          {this.props.mapType === 'counties' ? (
+            <g className="state-outlines">
+              <StateOutlines path={path} />
+            </g>
+          ) : null}
         </svg>
       </div>
     );
   }
 }
+
+USMap.propTypes = {
+  mapData: PropTypes.arrayOf(PropTypes.object),
+  mapType: PropTypes.string,
+  colors: PropTypes.arrayOf(PropTypes.string),
+  domain: PropTypes.arrayOf(PropTypes.string),
+  steps: PropTypes.number,
+  colorMode: PropTypes.string,
+};
 
 function mapStateToProps(state) {
   return {
@@ -134,7 +123,7 @@ function mapStateToProps(state) {
     scale: state.scale,
     colors: state.colors,
     colorMode: state.colorMode,
-    steps: state.steps
+    steps: state.steps,
   };
 }
 
