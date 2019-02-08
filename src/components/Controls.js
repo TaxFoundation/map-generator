e8211a7ff6aecc8629d638210f88a84380e3a0df
Tom VanAntwerp <tom@tomvanantwerp.com>
2019-02-08 14:09:35 -0500

    Convert Controls.js to use React hooks

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
@@ -13,36 +13,29 @@ const ControlsPanel = styled(Panel)`
   margin-right: 10px;
 `;
 
-class Controls extends Component {
-  constructor(props) {
-    super(props);
+const Controls = () => {
+  const [value, setValue] = useState(0);
 
-    this.state = {
-      value: 0,
-    };
-  }
-
-  handleChange = (event, value) => {
-    this.setState({ value });
-  };
-
-  render() {
-    return (
-      <ControlsPanel>
-        <PanelTypography variant="headline">Map Options</PanelTypography>
-        <AppBar position="static">
-          <Tabs onChange={this.handleChange} value={this.state.value}>
-            <Tab label="Data" />
-            <Tab label="Style" />
-            <Tab label="Output" />
-          </Tabs>
-        </AppBar>
-        {this.state.value === 0 && <DataControls />}
-        {this.state.value === 1 && <StyleControls />}
-        {this.state.value === 2 && <OutputControls />}
-      </ControlsPanel>
-    );
-  }
-}
+  return (
+    <ControlsPanel>
+      <PanelTypography variant="headline">Map Options</PanelTypography>
+      <AppBar position="static">
+        <Tabs
+          onChange={(e, v) => {
+            setValue(v);
+          }}
+          value={value}
+        >
+          <Tab label="Data" />
+          <Tab label="Style" />
+          <Tab label="Output" />
+        </Tabs>
+      </AppBar>
+      {value === 0 && <DataControls />}
+      {value === 1 && <StyleControls />}
+      {value === 2 && <OutputControls />}
+    </ControlsPanel>
+  );
+};
 
 export default Controls;
