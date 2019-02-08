9e592fb1fc170b6d1e59f29b91579aca7800aa6b
Tom VanAntwerp <tom@tomvanantwerp.com>
2019-02-08 14:13:02 -0500

    Convert ColorControls.js to use React Hooks

--- src/components/controls/ColorControls.js
+++ src/components/controls/ColorControls.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import MapGeneratorContext from '../../Context';
 import Select from '@material-ui/core/Select';
@@ -8,58 +8,49 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import PalettePreview from './PalettePreview';
 
-class ColorControls extends Component {
-  constructor(props) {
-    super(props);
+const ColorControls = props => {
+  const colorModes = ['lch', 'rgb', 'hsv'];
+  const context = useContext(MapGeneratorContext);
 
-    this.colorModes = ['lch', 'rgb', 'hsv'];
-  }
-
-  render() {
-    return (
-      <MapGeneratorContext.Consumer>
-        {context => (
-          <div className={this.props.className}>
-            <FormControl fullWidth>
-              <InputLabel htmlFor="color-palette">Color Palette</InputLabel>
-              <Select
-                input={<Input id="color-palette" />}
-                onChange={event => {
-                  context.updateState('colors', event.target.value);
-                  let newActives = { ...context.state.activeDataTypeColors };
-                  newActives[context.state.dataType] = event.target.value;
-                  context.updateState('activeDataTypeColors', newActives);
-                }}
-                renderValue={value => {
-                  return (
-                    <PalettePreview
-                      colorMode={context.state.colorMode}
-                      palette={value}
-                      steps={context.state.steps}
-                    />
-                  );
-                }}
-                value={context.state.colors}
-              >
-                {context.palettes[context.state.dataType].map(p => {
-                  return (
-                    <MenuItem key={`palette-${p.id}`} value={p.palette}>
-                      <PalettePreview
-                        mode={context.state.colorMode}
-                        palette={p.palette}
-                        steps={context.state.steps}
-                      />
-                    </MenuItem>
-                  );
-                })}
-              </Select>
-            </FormControl>
-          </div>
-        )}
-      </MapGeneratorContext.Consumer>
-    );
-  }
-}
+  return (
+    <div className={props.className}>
+      <FormControl fullWidth>
+        <InputLabel htmlFor="color-palette">Color Palette</InputLabel>
+        <Select
+          input={<Input id="color-palette" />}
+          onChange={event => {
+            context.updateState('colors', event.target.value);
+            let newActives = { ...context.state.activeDataTypeColors };
+            newActives[context.state.dataType] = event.target.value;
+            context.updateState('activeDataTypeColors', newActives);
+          }}
+          renderValue={value => {
+            return (
+              <PalettePreview
+                colorMode={context.state.colorMode}
+                palette={value}
+                steps={context.state.steps}
+              />
+            );
+          }}
+          value={context.state.colors}
+        >
+          {context.palettes[context.state.dataType].map(p => {
+            return (
+              <MenuItem key={`palette-${p.id}`} value={p.palette}>
+                <PalettePreview
+                  mode={context.state.colorMode}
+                  palette={p.palette}
+                  steps={context.state.steps}
+                />
+              </MenuItem>
+            );
+          })}
+        </Select>
+      </FormControl>
+    </div>
+  );
+};
 
 ColorControls.propTypes = {
   className: PropTypes.string,
