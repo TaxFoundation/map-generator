40421f8efbf8f0bd49f7bbd4debf5bb917f04343
Tom VanAntwerp <tom@tomvanantwerp.com>
2019-03-28 16:51:06 -0400

    Create header styles

--- src/sections/Header.js
+++ src/sections/Header.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Header = () => <div />;
+const StyledHeader = styled.header`
+  background-color: #0094ff;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+  color: #fff;
+  font-size: 3rem;
+  font-weight: 300;
+  grid-column: 1 / 4;
+  text-align: center;
+`;
+
+const Header = () => (
+  <StyledHeader>
+    <h1>Tax Foundation Map Generator</h1>
+  </StyledHeader>
+);
 
 export default Header;
