59c68cb6882f56e98db0c1a44a3cc7a95ff469a8
Tom VanAntwerp <tom@tomvanantwerp.com>
2019-04-03 16:04:06 -0400

    Conditionally swap map components based on geographic type

--- src/components/menus/Data.js
+++ src/components/menus/Data.js
@@ -6,19 +6,44 @@ const Data = () => {
   const { data, updateData } = useContext(DataContext);
   return (
     <form>
-      <label htmlFor="map-data-type">What type of data is this?</label>
-      <select
-        name="map-data-type"
-        id="map-data-type"
-        value={data.mapDataType}
-        onChange={e =>
-          updateData({ do: 'update', id: 'mapDataType', value: e.target.value })
-        }
-      >
-        <option value="sequential">Sequential</option>
-        <option value="divergent">Divergent</option>
-        <option value="qualitative">Qualitative</option>
-      </select>
+      <div>
+        <label htmlFor="map-geo-type">What type of map is this?</label>
+        <select
+          name="map-geo-type"
+          id="map-geo-type"
+          value={data.mapGeographyType}
+          onChange={e =>
+            updateData({
+              do: 'update',
+              id: 'mapGeographyType',
+              value: e.target.value,
+            })
+          }
+        >
+          <option value="states">US States</option>
+          <option value="counties">US Counties</option>
+          <option value="europe">European Nations</option>
+        </select>
+      </div>
+      <div>
+        <label htmlFor="map-data-type">What type of data is this?</label>
+        <select
+          name="map-data-type"
+          id="map-data-type"
+          value={data.mapDataType}
+          onChange={e =>
+            updateData({
+              do: 'update',
+              id: 'mapDataType',
+              value: e.target.value,
+            })
+          }
+        >
+          <option value="sequential">Sequential</option>
+          <option value="divergent">Divergent</option>
+          <option value="qualitative">Qualitative</option>
+        </select>
+      </div>
       <button
         type="button"
         onClick={() => {
--- src/sections/Map.js
+++ src/sections/Map.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
+import { DataContext } from '../contexts/DataContext';
 import Panel from '../components/ui/Panel';
 import PanelHeading from '../components/ui/PanelHeading';
 import States from '../components/maps/States';
 
-const Map = () => (
-  <Panel columns={2}>
-    <PanelHeading>Map</PanelHeading>
-    <States />
-  </Panel>
-);
+const Map = () => {
+  const { data: mapContext } = useContext(DataContext);
+  const { mapGeographyType: geography } = mapContext;
+
+  return (
+    <Panel columns={2}>
+      <PanelHeading>Map</PanelHeading>
+      {getMap(geography)}
+    </Panel>
+  );
+};
+
+const getMap = geography => {
+  switch (geography) {
+    case 'states':
+      return <States />;
+    default:
+      return null;
+  }
+};
 
 export default Map;
