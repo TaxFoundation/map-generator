8ce7af3f6a1faa2d9f2f1d65c60be3f43c74ed78
Tom VanAntwerp <tom@tomvanantwerp.com>
2019-04-04 14:46:49 -0400

    Menu tabs like the old version

--- src/components/menus/Style.js
+++ src/components/menus/Style.js
@@ -1,71 +1,61 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 
 import { DataContext } from '../../contexts/DataContext';
-import CollapsibleMenu from '../ui/CollapsibleMenu';
 
 const Style = () => {
-  const [collapse, setCollapse] = useState(false);
   const { data, updateData } = useContext(DataContext);
   return (
-    <>
-      <h3>
-        Style{' '}
-        <span onClick={() => setCollapse(!collapse)}>
-          [{collapse ? 'show' : 'hide'}]
-        </span>
-      </h3>
-      <CollapsibleMenu collapse={collapse}>
-        <form>
-          <div>
-            <label htmlFor="number-format">
-              How should the values be formatted?
-            </label>
-            <input
-              checked={data.dataType === 'number'}
-              onChange={e =>
-                updateData({
-                  id: 'dataType',
-                  value: e.target.value,
-                })
-              }
-              id="number-format-1"
-              name="number-format"
-              type="radio"
-              value="number"
-            />
-            <label htmlFor="number-format-1">Number</label>
-            <input
-              checked={data.dataType === 'percent'}
-              onChange={e =>
-                updateData({
-                  id: 'dataType',
-                  value: e.target.value,
-                })
-              }
-              id="number-format-2"
-              name="number-format"
-              type="radio"
-              value="percent"
-            />
-            <label htmlFor="number-format-2">Percent</label>
-            <input
-              checked={data.dataType === 'dollar'}
-              onChange={e =>
-                updateData({
-                  id: 'dataType',
-                  value: e.target.value,
-                })
-              }
-              id="number-format-3"
-              name="number-format"
-              type="radio"
-              value="dollar"
-            />
-            <label htmlFor="number-format-13">Dollar</label>
-          </div>
-        </form>
-      </CollapsibleMenu>
-    </>
+    <div>
+      <form>
+        <div>
+          <label htmlFor="number-format">
+            How should the values be formatted?
+          </label>
+          <input
+            checked={data.dataType === 'number'}
+            onChange={e =>
+              updateData({
+                id: 'dataType',
+                value: e.target.value,
+              })
+            }
+            id="number-format-1"
+            name="number-format"
+            type="radio"
+            value="number"
+          />
+          <label htmlFor="number-format-1">Number</label>
+          <input
+            checked={data.dataType === 'percent'}
+            onChange={e =>
+              updateData({
+                id: 'dataType',
+                value: e.target.value,
+              })
+            }
+            id="number-format-2"
+            name="number-format"
+            type="radio"
+            value="percent"
+          />
+          <label htmlFor="number-format-2">Percent</label>
+          <input
+            checked={data.dataType === 'dollar'}
+            onChange={e =>
+              updateData({
+                id: 'dataType',
+                value: e.target.value,
+              })
+            }
+            id="number-format-3"
+            name="number-format"
+            type="radio"
+            value="dollar"
+          />
+          <label htmlFor="number-format-13">Dollar</label>
+        </div>
+      </form>
+    </div>
   );
 };
 
--- src/sections/Menu.js
+++ src/sections/Menu.js
@@ -1,16 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import Panel from '../components/ui/Panel';
 import PanelHeading from '../components/ui/PanelHeading';
 import Data from '../components/menus/Data';
 import Style from '../components/menus/Style';
+import Output from '../components/menus/Output';
 
-const Menu = () => (
-  <Panel>
-    <PanelHeading>Menu</PanelHeading>
-    <Data />
-    <Style />
-  </Panel>
-);
+const MenuSelection = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+
+  & > * {
+    flex: 1;
+  }
+`;
+
+const MenuOption = styled.h3`
+  background-color: ${props => (props.selected ? '#0094ff' : '#fff')};
+  border: 1px solid ${props => (props.selected ? '#0094ff' : '#fff')};
+  border-radius: 4px;
+  color: ${props => (props.selected ? '#fff' : '#333')};
+  cursor: pointer;
+  font-size: 1.6rem;
+  padding: 0.5rem;
+  text-align: center;
+`;
+
+const getMenu = id => {
+  switch (id) {
+    case 'data':
+      return <Data />;
+    case 'style':
+      return <Style />;
+    case 'output':
+      return <Output />;
+    default:
+      return null;
+  }
+};
+
+const Menu = () => {
+  const [menuSelection, setMenuSelection] = useState('data');
+  return (
+    <Panel>
+      <PanelHeading>Options</PanelHeading>
+      <MenuSelection>
+        <MenuOption
+          onClick={() => setMenuSelection('data')}
+          selected={menuSelection === 'data'}
+        >
+          Data
+        </MenuOption>
+        <MenuOption
+          onClick={() => setMenuSelection('style')}
+          selected={menuSelection === 'style'}
+        >
+          Style
+        </MenuOption>
+        <MenuOption
+          onClick={() => setMenuSelection('output')}
+          selected={menuSelection === 'output'}
+        >
+          Output
+        </MenuOption>
+      </MenuSelection>
+      {getMenu(menuSelection)}
+    </Panel>
+  );
+};
 
 export default Menu;
