b945eed8c24da37e95029a838ba0d453f8d91ab8
Tom VanAntwerp <tom@tomvanantwerp.com>
2019-04-16 14:53:04 -0400

    Links for sample CSVs

--- src/sections/Map.js
+++ src/sections/Map.js
@@ -4,45 +4,73 @@ import styled from 'styled-components';
 import { DataContext } from '../contexts/DataContext';
 import Panel from '../components/ui/Panel';
 import PanelHeading from '../components/ui/PanelHeading';
+import HR from '../components/ui/HR';
 import States from '../components/maps/States';
 
 const Check = styled.span`
   color: ${props => (props.valid ? 'green' : 'red')};
 `;
 
+function getMap(geography) {
+  switch (geography) {
+    case 'states':
+      return <States />;
+    default:
+      return null;
+  }
+}
+
 const DataValidation = () => {
   const { data: mapContext } = useContext(DataContext);
 
   return (
-    <div>
-      <h3>Waiting for the following to be set...</h3>
-      <ul>
-        <li>
-          <Check valid={mapContext.rawData !== null}>
-            {mapContext.rawData ? '✓' : '✗'}
-          </Check>{' '}
-          Data Uploaded
-        </li>
-        <li>
-          <Check valid={mapContext.idColumn !== null}>
-            {mapContext.idColumn ? '✓' : '✗'}
-          </Check>{' '}
-          ID Chosen
-        </li>
-        <li>
-          <Check valid={mapContext.valueColumn !== null}>
-            {mapContext.valueColumn ? '✓' : '✗'}
-          </Check>{' '}
-          Value Chosen
-        </li>
-        <li>
-          <Check valid={mapContext.mapGeographyType !== null}>
-            {mapContext.mapGeographyType ? '✓' : '✗'}
-          </Check>{' '}
-          Geography Type Chosen
-        </li>
-      </ul>
-    </div>
+    <>
+      <div>
+        <h3>Waiting for the following to be set...</h3>
+        <ul>
+          <li>
+            <Check valid={mapContext.rawData !== null}>
+              {mapContext.rawData ? '✓' : '✗'}
+            </Check>{' '}
+            Data Uploaded
+          </li>
+          <li>
+            <Check valid={mapContext.idColumn !== null}>
+              {mapContext.idColumn ? '✓' : '✗'}
+            </Check>{' '}
+            ID Chosen
+          </li>
+          <li>
+            <Check valid={mapContext.valueColumn !== null}>
+              {mapContext.valueColumn ? '✓' : '✗'}
+            </Check>{' '}
+            Value Chosen
+          </li>
+          <li>
+            <Check valid={mapContext.mapGeographyType !== null}>
+              {mapContext.mapGeographyType ? '✓' : '✗'}
+            </Check>{' '}
+            Geography Type Chosen
+          </li>
+        </ul>
+      </div>
+      <HR />
+      <div>
+        <h3>Download sample CSV files.</h3>
+        <ul>
+          <li>
+            <a href="csv/states.csv" download>
+              US States
+            </a>
+          </li>
+          <li>
+            <a href="csv/europe.csv" download>
+              Europe
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
@@ -62,15 +90,6 @@ const Map = () => {
       {dataReady ? getMap(geography) : <DataValidation />}
     </Panel>
   );
-};
-
-const getMap = geography => {
-  switch (geography) {
-    case 'states':
-      return <States />;
-    default:
-      return <div>Waiting for map geography type selection.</div>;
-  }
 };
 
 export default Map;
