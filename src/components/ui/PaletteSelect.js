6a808b5d6f96b498e71a1b5465619271062ac20a
Tom VanAntwerp <tom@tomvanantwerp.com>
2019-06-12 11:34:46 -0400

    Add correct change palette button styles

--- src/components/ui/PaletteSelect.js
+++ src/components/ui/PaletteSelect.js
@@ -10,6 +10,7 @@ import {
 } from '../../data/colorPalette';
 import { colorScale } from '../../helpers';
 import Modal from './Modal';
+import Label from './Label';
 
 function choosePalette(type) {
   switch (type) {
@@ -38,8 +39,15 @@ const Palette = styled.li`
 
 const PaletteChunk = styled.div`
   background-color: ${props => props.bg};
-  height: '20px';
-  width: ${props => props.width};
+  height: 20px;
+`;
+
+const StyledButton = styled.button`
+  border: 0;
+  cursor: pointer;
+  display: block;
+  padding: 0;
+  width: 100%;
 `;
 
 const PaletteList = ({ palettes, close }) => {
@@ -77,11 +85,31 @@ const PaletteSelect = () => {
   const { data } = useContext(DataContext);
   const [open, setOpen] = useState(false);
 
+  const currentPalette = choosePalette(data.numericDataType).find(
+    p => data.paletteId === p.id
+  );
+
   return (
     <div>
-      <button type="button" onClick={() => setOpen(!open)}>
-        Change Palette
-      </button>
+      <Label htmlFor="palette">Change Current Palette</Label>
+      <StyledButton type="button" id="palette" onClick={() => setOpen(!open)}>
+        <Palette>
+          {[...Array(data.bins).keys()].map((d, i) => (
+            <PaletteChunk
+              bg={colorScale(
+                currentPalette.palette,
+                [0, data.bins - 1],
+                d,
+                data.bins,
+                data.colorMode
+              )}
+              key={`current-palette-${data.numericDataType}-${
+                data.paletteId
+              }-${i}`}
+            />
+          ))}
+        </Palette>
+      </StyledButton>
       {open && (
         <Modal>
           <h1>Test Palette Select</h1>
