4e6653d90320bf4392bf5227e085892003cf8383
Tom VanAntwerp <tom@tomvanantwerp.com>
2020-04-14 12:57:35 -0400

    Add prop types for PaletteList

--- src/components/ui/PaletteSelect.js
+++ src/components/ui/PaletteSelect.js
@@ -123,7 +123,7 @@ const PaletteSelect = () => {
         <Modal title="Choose a New Palette" close={() => setOpen(false)}>
           <PaletteList
             data={data}
-            updateDate={updateData}
+            updateData={updateData}
             type={paletteType}
             palettes={choosePalette(paletteType)}
             close={() => setOpen(false)}
@@ -133,6 +133,14 @@ const PaletteSelect = () => {
       <Toggle id="paletteDirectionFlipped" label="Flip palette direction?" />
     </div>
   );
+};
+
+PaletteList.propTypes = {
+  palettes: PropTypes.array,
+  type: PropTypes.string,
+  close: PropTypes.func,
+  data: PropTypes.object,
+  updateData: PropTypes.func,
 };
 
 export default PaletteSelect;
