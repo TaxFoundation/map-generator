81d874483f0046e11ea83e8b7fd2c696cbc79cc9
Tom VanAntwerp <tom@tomvanantwerp.com>
2020-04-14 12:23:20 -0400

    Check string values if they match a percentage then convert to decimal

--- src/contexts/reducers/ValueColumnReducer.js
+++ src/contexts/reducers/ValueColumnReducer.js
@@ -1,32 +1,39 @@
-import { isNumericData, generateMapData } from '../../helpers';
+import {
+  isNumericData,
+  isPercentageStringData,
+  valueConvert,
+  generateMapData,
+} from '../../helpers';
 
 const ValueColumnReducer = (state, action) => {
-  if (state.idColumn) {
-    const newMapData = generateMapData(
+  if (state.idColumn && state.idColumn !== '¯\\_(ツ)_/¯') {
+    const mapData = generateMapData(
       state.rawData,
       state.idColumn,
       action.value,
       state.rankColumn
     );
-    let newIsNumeric = false;
-    let newMin = state.min;
-    let newMax = state.max;
-    let newDomain = state.domain;
-    if (isNumericData(newMapData)) {
-      newIsNumeric = true;
-      const values = newMapData.map(d => Number(d.value));
-      newMin = Math.min(...values);
-      newMax = Math.max(...values);
-      newDomain = [newMin, newMax];
+    let isNumeric = false;
+    let { min, max, domain, formatType } = state;
+    if (isNumericData(mapData)) {
+      isNumeric = true;
+      const values = mapData.map(d => valueConvert(d.value));
+      min = Math.min(...values);
+      max = Math.max(...values);
+      domain = [min, max];
+    }
+    if (isPercentageStringData(state.rawData.map(row => row[action.value]))) {
+      formatType = 'percent';
     }
     return {
       ...state,
       valueColumn: action.value,
-      mapData: newMapData,
-      isNumeric: newIsNumeric,
-      min: newMin,
-      max: newMax,
-      domain: newDomain,
+      mapData,
+      isNumeric,
+      min,
+      max,
+      domain,
+      formatType,
     };
   }
   return { ...state, valueColumn: action.value };
